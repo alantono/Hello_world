c88c22fc560b0fe0613dca99f87f1bfd5b0f20ec
Alexey <antonov1972@yandex.ru>
2022-10-16 11:51:52 +0300

    compare

---
+++ Example006_compare/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 5;
+int b = 6;
+int c = 9;
+int d = 3;
+int e = 7;
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.Write("Максимальное значение= ");
+Console.WriteLine(max);
+    
+  
